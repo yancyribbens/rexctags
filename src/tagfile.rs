0f23f203ef5f776163415c7b51bae40fdc0878a6
Yancy <email@yancy.lol>
2022-01-06 14:09:27 +0100

    Simplify structure by operating only on raw tag lines

--- src/tagfile.rs
+++ src/tagfile.rs
@@ -1,41 +1,39 @@
 use std::error::Error;
-use crate::tag::Tag;
 
 pub struct TagFile {
     _version: u8, // !_TAG_FILE_FORMAT
     _sorted: u8, // !_TAG_FILE_SORTED
-    tags: Vec<Tag>
+    entries: Vec<String>
 }
 
 impl TagFile {
     pub fn new(raw_file_tags: String) -> Result<TagFile, Box<dyn Error>> {
-        let tags = TagFile::parse(raw_file_tags).unwrap();
+        let entries = TagFile::parse(raw_file_tags).unwrap();
         let _version = 2;
         let _sorted = 1;
-        Ok(TagFile { _version, _sorted, tags })
+        Ok(TagFile { _version, _sorted, entries })
     }
 
     pub fn is_file_header(line: &String) -> bool {
         line.as_bytes()[0] == 33u8
     }
 
-    pub fn parse(raw_tag_file: String) -> Result<Vec<Tag>, Box<dyn Error>> {
-        let mut tags: Vec<Tag> = Vec::new();
+    pub fn parse(raw_tag_file: String) -> Result<Vec<String>, Box<dyn Error>> {
+        let mut tags: Vec<String> = Vec::new();
         let lines: Vec<String> = raw_tag_file.split("\n").map(|s| s.to_string()).collect();
         for line in lines {
             if !TagFile::is_file_header(&line) {
-                let tag = Tag::new(line).unwrap(); 
-                tags.push(tag);
+                tags.push(line);
             }
         }
         Ok(tags)
     }
 
-    //TODO dedup (and test)
-    pub fn merge(mut self, mut source_tag: TagFile) -> Result<(), Box<dyn Error>> {
-        self.tags.append(&mut source_tag.tags);
-        self.tags.sort_by(|m, n| m.tagname.cmp(&n.tagname));
-        Ok(()) 
+    pub fn merge(mut self, mut source_tag: TagFile) -> Result<TagFile, Box<dyn Error>> {
+        self.entries.append(&mut source_tag.entries);
+        self.entries.sort_by(|m, n| m.cmp(&n));
+        self.entries.dedup();
+        Ok(self) 
     }
 }
 
@@ -47,7 +45,7 @@ mod tests {
         let tag_file_format_header = r#"!_TAG_FILE_FORMAT	2   /optional comment/"#;
         let tag_file_sorted_header = r#"!_TAG_FILE_SORTED	1			/0=unsorted, 1=sorted/"#;
         let tag_line_alpha = r#"asdf	sub.cc	/^asdf()$/;"	new_field:some\svalue	file:"#;
-        let tag_line_beta = r#"inc	sub.cc	/^inc()$/;"	file: class:PipeBuf\n"#;
+        let tag_line_beta = r#"foo_t	sub.h	/^typedef foo_t$/;"	kind:t"#;
         let tag_file = format!("{}\n{}\n{}\n{}",
             tag_file_format_header,
             tag_file_sorted_header,
@@ -60,25 +58,25 @@ mod tests {
     fn tag_file_beta() -> String {
         let tag_file_format_header = r#"!_TAG_FILE_FORMAT	2   /optional comment/"#;
         let tag_file_sorted_header = r#"!_TAG_FILE_SORTED	1			/0=unsorted, 1=sorted/"#;
-        let tag_line_alpha = r#"foo_t	sub.h	/^typedef foo_t$/;"	kind:t"#;
-        let tag_line_beta = r#"func3	sub.p	/^func3()$/;"	function:/func1/func2	file:"#;
-		let tag_line_gamma = r#"getflag	sub.c	/^getflag(arg)$/;"	kind:f	file:"#;
-        let tag_file = format!("{}\n{}\n{}\n{}\n{}",
+        let tag_line_alpha = r#"getflag	sub.c	/^getflag(arg)$/;"	kind:f	file:"#;
+        let tag_line_beta = r#"foo_t	sub.h	/^typedef foo_t$/;"	kind:t"#;
+        let tag_file = format!("{}\n{}\n{}\n{}",
             tag_file_format_header,
             tag_file_sorted_header,
             tag_line_alpha,
-            tag_line_beta,
-			tag_line_gamma
-		);
-		tag_file
+            tag_line_beta
+        );
+        tag_file
     }
 
     #[test]
-    fn test_tag_file() {
-        let raw_tag_file = tag_file_alpha();
-        let tagfile = TagFile::new(raw_tag_file).unwrap();
-        assert_eq!("asdf", tagfile.tags[0].tagname);
-        assert_eq!("inc", tagfile.tags[1].tagname);
-    }
+    fn test_merge() {
+        let alpha = TagFile::new(tag_file_alpha()).unwrap();
+        let beta = TagFile::new(tag_file_beta()).unwrap();
+        let merged = alpha.merge(beta).unwrap();
 
+        assert_eq!(merged.entries[0], r#"asdf	sub.cc	/^asdf()$/;"	new_field:some\svalue	file:"#);
+        assert_eq!(merged.entries[1], r#"foo_t	sub.h	/^typedef foo_t$/;"	kind:t"#);
+        assert_eq!(merged.entries[2], r#"getflag	sub.c	/^getflag(arg)$/;"	kind:f	file:"#);
+    }
 }
